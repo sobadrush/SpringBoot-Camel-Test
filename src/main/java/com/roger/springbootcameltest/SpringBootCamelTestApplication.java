da471ff22e107747aff0b201637f81eb8a54089e
Roger Lo <sobadrush@icloud.com>
2023-03-09 15:14:59 +0800

    init commit

---
+++ src/main/java/com/roger/springbootcameltest/SpringBootCamelTestApplication.java
@@ -0,0 +1,13 @@
+package com.roger.springbootcameltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCamelTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roger/springbootcameltest/SpringBootCamelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roger.springbootcameltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCamelTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
